0ffb1e110f8d75d74d36b7ed3dc491c948e74e86
sachinm78 <sachinm78@gmail.com>
2020-02-19 13:33:13 -0600

    created shofCoffees function to display coffee cards

--- js/app.js
+++ js/app.js
@@ -10,3 +10,20 @@ const coffees = [
   { name: "Veritatis", image: "images/coffee8.jpg" },
   { name: "Accusantium", image: "images/coffee9.jpg" },
 ]
+
+const showCoffees = () => {
+    let output = ""
+    coffees.forEach(
+      ({ name, image }) =>
+        (output += `
+                <div class="card">
+                  <img class="card--avatar" src=${image} />
+                  <h1 class="card--title">${name}</h1>
+                  <a class="card--link" href="#">Taste</a>
+                </div>
+                `)
+    )
+    container.innerHTML = output
+}
+  
+document.addEventListener("DOMContentLoaded", showCoffees)
